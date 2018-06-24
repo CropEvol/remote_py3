e76dd84afe9e82802c3b97195d3f95718f58d405
堺俊之 <toshi6661024@gmail.com>
2018-06-24 16:01:24 +0900

    add next lecture

--- ILAS/remote_ncbi.py
+++ ILAS/remote_ncbi.py
@@ -38,30 +38,31 @@ def ncbi_blast(blast, sequence):
             continue
 
         if re.search("Status=FAILED", str_check_response):
-            print("Search failed...")
-            sys.exit()
+            print("Search failed...\n")
+            break
 
         if re.search("Status=UNKNOWN", str_check_response):
-            print("something happened...")
-            sys.exit()
+            print("something happened...\n")
+            break
 
         if re.search("Status=READY", str_check_response):
             if re.search("hereAreHits=yes", str_check_response):
                 print("BLAST search finished.")
+                
+                get_params = {
+                    'CMD': 'Get',
+                    'FORMAT_TYPE': 'Text',
+                    'RID': rid,
+                }
+                get_response = requests.get(base_url, params=get_params)
+                f = open("{0}.txt".format(rid), 'w')
+                f.write(get_response.text)
+                f.close()
+                print(get_response.text)
                 break
             else:
-                print("No hits found...")
-                sys.exit()
-
-    get_params = {
-        'CMD': 'Get',
-        'FORMAT_TYPE': 'Text',
-        'RID': rid,
-    }
-    get_response = requests.get(base_url, params=get_params)
-    f = open("{0}.txt".format(rid), 'w')
-    f.write(get_response.text)
-    f.close()
+                print("No hits found...\n")
+                break
 
 
 if __name__ == "__main__":
---
+++ ILAS/sample.py
@@ -0,0 +1,5 @@
+def kansuu_1():
+    print("kansuu_1と言う関数を動かした\n")
+    
+def kansuu_2(x):
+    print("引数は", x, "です")
